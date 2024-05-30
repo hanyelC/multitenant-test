360882307933694a9ebc3b20612de153f6344da4
Hanyel Chamon <hanyel.chamon@gmail.com>
2024-05-30 20:20:11 -0300

    delete this crap

--- go.mod
+++
@@ -1,5 +0,0 @@
-module hanyelC/multitenant-test
-
-go 1.22.0
-
-require github.com/lib/pq v1.10.9
--- sql/queries/create-users-table.sql
+++
@@ -1,5 +0,0 @@
--- name: CreateUsersTable :exec
-CREATE TABLE "users" (
-  "id" serial NOT NULL UNIQUE PRIMARY KEY,
-  "email" text NOT NULL UNIQUE
-);
